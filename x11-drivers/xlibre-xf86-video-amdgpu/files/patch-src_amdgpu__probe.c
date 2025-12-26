a302348553a1a26d215abf4df7039b26180f61b1
b-aaz <85005689+b-aaz@users.noreply.github.com>
2025-12-26 00:41:52 +0000

    amdgpu: Reverted the removal of a patch.
    This was originally a remote patch, that was removed in previous
    commits.
    It appears that this section of the code causes a segfault on the
    server, so it is better be left removed.

--- x11-drivers/xlibre-xf86-video-amdgpu/files/patch-src_amdgpu__probe.c
+++ x11-drivers/xlibre-xf86-video-amdgpu/files/patch-src_amdgpu__probe.c
@@ -1,11 +1,27 @@
---- src/amdgpu_probe.c.orig	2025-12-23 20:17:36 UTC
+--- src/amdgpu_probe.c.orig	2025-12-26 00:17:12 UTC
 +++ src/amdgpu_probe.c
-@@ -92,7 +92,7 @@ static Bool amdgpu_kernel_mode_enabled(ScrnInfoPtr pSc
- 	int ret = drmCheckModesettingSupported(busIdString);
+@@ -86,24 +86,6 @@ static Bool amdgpu_kernel_mode_enabled(ScrnInfoPtr pSc
  
- 	if (ret) {
+ static Bool amdgpu_kernel_mode_enabled(ScrnInfoPtr pScrn)
+ {
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+-	AMDGPUEntPtr pAMDGPUEnt = AMDGPUEntPriv(pScrn);
+-	const char *busIdString = pAMDGPUEnt->busid;
+-	int ret = drmCheckModesettingSupported(busIdString);
+-
+-	if (ret) {
 -		if (xf86LoadKernelModule("amdgpukms"))
-+		if (xf86LoadKernelModule("amdgpu"))
- 			ret = drmCheckModesettingSupported(busIdString);
- 	}
- 	if (ret) {
+-			ret = drmCheckModesettingSupported(busIdString);
+-	}
+-	if (ret) {
+-		xf86DrvMsgVerb(pScrn->scrnIndex, X_INFO, 0,
+-			       "[KMS] drm report modesetting isn't supported.\n");
+-		return FALSE;
+-	}
+-
+-#endif
+-	xf86DrvMsgVerb(pScrn->scrnIndex, X_INFO, 0,
+-		       "[KMS] Kernel modesetting enabled.\n");
+ 	return TRUE;
+ }
+ 
