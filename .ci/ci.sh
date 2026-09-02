101a2c17638a68521a824b37557b62f7d13d7f04
b-aaz <85005689+b-aaz@users.noreply.github.com>
2026-09-02 16:37:47 +0000

    run 11

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -2,8 +2,10 @@
 
 ls
 cd ./.ci/
-. image-fetcher.sh
+. ./image-fetcher.sh
+
 image_fetch
+ls
 ./vm-exp/vm.exp \
 	./vm-scripts/bootloader_cmds \
 	./vm-scripts/singleuser_fbsd_cmds \
@@ -12,4 +14,3 @@ image_fetch
 
 ssh-keyscan -p 10022 127.0.0.1
 ssh-keyscan -p 10022 root@127.0.0.1 uname -a
-ls
