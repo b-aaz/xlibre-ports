8b6d0bef48cb5566aad9c37c88121bd2e308bb1c
b-aaz <85005689+b-aaz@users.noreply.github.com>
2026-09-03 15:48:25 +0000

    run 31

--- .ci/dep-installer.sh
+++ .ci/dep-installer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 dep_install () {
-	apt-get update
-	apt-get install \
+	apt-get update  -q -o=Dpkg::Use-Pty=0
+	apt-get install -q -o=Dpkg::Use-Pty=0 \
 		expect \
 		qemu-utils \
 		qemu-system-x86
