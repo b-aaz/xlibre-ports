c2f50e30abbc50ee84fec78ee9ae2d6b8e51cc27
b-aaz <85005689+b-aaz@users.noreply.github.com>
2026-09-03 14:56:26 +0000

    run 25

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -2,18 +2,25 @@
 
 cd /opt/ci-run/
 cd ./.ci/
-. ./image-fetcher.sh
-. ./dep-installer.sh
+s_dir=${0%/*}; [ "$s_dir" = "$0" ] && s_dir='.'
+. "$s_dir/image-fetcher.sh"
+. "$s_dir/dep-installer.sh"
+. "$s_dir/print-utils.sh"
 
+section VM-PRERUN
 ssh-keygen -f ~/.ssh/id_ed25519 -t ed25519 -N '' &
 dep_install &
 image_fetch &
 wait
+section_end
+
+section VM-SETUP
 ./vm-exp/vm.exp \
 	./vm-scripts/bootloader_cmds \
 	./vm-scripts/singleuser_fbsd_cmds \
 	./vm-scripts/normal_cmds \
 	./vm-runner.sh
-
 ssh-keyscan -p 10022 127.0.0.1 >> ~/.ssh/known_hosts
+section_end
+
 ssh -p 10022 root@127.0.0.1 uname -a
