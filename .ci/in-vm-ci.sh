1ae9ddacf06fbf53da047a0f8759c463f15b5cde
b-aaz <85005689+b-aaz@users.noreply.github.com>
2026-09-04 00:23:37 +0000

    run 41

--- .ci/in-vm-ci.sh
+++ .ci/in-vm-ci.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
-echo Hello form VM.
-echo
-printenv
-uname -A
-
-
 [ "${GITHUB_ACTIONS}" = "true" ] && echo '::group::Inner Clone'
 mkdir -p /opt/ci-run/
 fetch -o - $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/tarball/$GITHUB_REF | tar -xvz --strip-components=1 -C /opt/ci-run/
 [ "${GITHUB_ACTIONS}" = "true" ] && echo '::endgroup::'
 
-ls /opt/ci-run/
+cd /opt/ci-run/
+. ./.ci/print-utils
+
+case "$(uname -s)" in
+	FreeBSD*)
+		PORTS_REPO="freebsd/freebsd-ports"
+		PORTS_BRANCH="refs/heads/2026Q2"
+		;;
+	DragonFly*)
+		PORTS_REPO="DragonFlyBSD/DeltaPorts"
+		PORTS_BRANCH="refs/heads/master"
+		;;
+	*)
+		exit 1
+		;;
+esac
+
+
+
+section CLONE-PORTS
+mkdir -p /usr/ports/
+rm -rf   /usr/ports/*
+fetch -o - $GITHUB_API_URL/repos/$PORTS_REPO/tarball/$PORTS_BRANCH | tar -xvz --strip-components=1 -C /usr/ports
+section_end
+
+ls /usr/ports/
