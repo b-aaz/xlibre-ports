d9fa66e68789b76fd01be3d4a3a0ac4472d4b6b4
b-aaz <85005689+b-aaz@users.noreply.github.com>
2026-09-04 15:36:05 +0000

    run 44

--- .ci/in-vm-ci.sh
+++ .ci/in-vm-ci.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-[ "${GITHUB_ACTIONS}" = "true" ] && echo '::group::Inner Clone'
+[ "${GITHUB_ACTIONS}" = "true" ] && echo '::group::INNER-CLONE'
 mkdir -p /opt/ci-run/
-fetch -o - $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/tarball/$GITHUB_REF | tar -xvz --strip-components=1 -C /opt/ci-run/
+fetch -o - "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/tarball/$GITHUB_REF" | \
+       	tar -xvz --strip-components=1 -C /opt/ci-run/
 [ "${GITHUB_ACTIONS}" = "true" ] && echo '::endgroup::'
 
 cd /opt/ci-run/
@@ -12,10 +13,14 @@ case "$(uname -s)" in
 	FreeBSD*)
 		PORTS_REPO="freebsd/freebsd-ports"
 		PORTS_BRANCH="refs/heads/2026Q2"
+		PORTS_DIR="/usr/ports"
+		FLITTERED_DEPS='x11/plasma6-plasma\|x11/plasma6-plasma-desktop'
 		;;
 	DragonFly*)
-		PORTS_REPO="DragonFlyBSD/DeltaPorts"
+		PORTS_REPO="DragonFlyBSD/dports"
 		PORTS_BRANCH="refs/heads/master"
+		PORTS_DIR="/usr/dports"
+		FLITTERED_DEPS='ports-mgmt/pkg'
 		;;
 	*)
 		exit 1
@@ -25,9 +30,64 @@ esac
 
 
 section CLONE-PORTS
-mkdir -p /usr/ports/
-rm -rf   /usr/ports/*
-fetch -o - $GITHUB_API_URL/repos/$PORTS_REPO/tarball/$PORTS_BRANCH | tar -xvz --strip-components=1 -C /usr/ports
+mkdir -p "$PORTS_DIR"
+rm -rf   "$PORTS_DIR/*"
+fetch -o - "$GITHUB_API_URL/repos/$PORTS_REPO/tarball/$PORTS_BRANCH" | \
+	tar -xz --strip-components=1 -C "$PORTS_DIR"
 section_end
 
-ls /usr/ports/
+section PORTS-PATCH
+{ {
+		patch -N "${PORTS_DIR}/Mk/bsd.port.subdir.mk" <\
+		       	./.ci/bsd.port.subdir.mk.patch
+	} || true }
+
+{ {
+		patch -N "${PORTS_DIR}/Mk/bsd.port.mk" < ./.ci/bsd.port.mk.patch
+	} || true }
+section_end
+
+section MAKE-CONFIG
+cat > /etc/make.conf << EOF
+OVERLAYS=/opt/ci-run/
+BATCH=yes
+PACKAGES=/opt/pkgs/
+EOF
+section_end
+
+section RUN-DEP-INSTALL
+make run-depends-list |\
+	sort |\
+	uniq |\
+	grep -v '^==\|xlibre' |\
+	awk -F "/" '{print $(NF-1) "/" $NF}' |\
+	grep -v "$NOT_TO_INSTALL_DEPS" |\
+	xargs pkg-static install -y
+section_end
+
+section BUILD-DEP-INSTALL
+make build-depends-list |\
+	sort |\
+	uniq |\
+	grep -v '^==\|xlibre' |\
+	awk -F "/" '{print $(NF-1) "/" $NF}' |\
+	grep -v "$NOT_TO_INSTALL_DEPS" |\
+	xargs pkg-static install -y
+section_end
+
+section STAGE
+make stage
+section_end
+
+section STAGE-QA
+make stage-qa
+section_end
+
+section CHECK-PLIST
+make check-plist
+section_end
+
+section PACKAGES
+make packages
+section_end
+
