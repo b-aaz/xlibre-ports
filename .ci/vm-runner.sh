815211c2757320057ffe343c40388a4186b98316
b-aaz <85005689+b-aaz@users.noreply.github.com>
2026-09-04 22:36:52 +0000

    run 52

--- .ci/vm-runner.sh
+++ .ci/vm-runner.sh
@@ -4,10 +4,10 @@
 
 export TERM=dumb # To maybe lessen the escape codes in the logs.
 nproc="${VM_RUNNER_CPUS:=$(getconf _NPROCESSORS_ONLN)}"
-mem="${VM_RUNNER_MEM:=7G}"
+mem="${VM_RUNNER_MEM:=8G}"
 img="${1:-${VM_RUNNER_IMG_FILE:=vm-img}}"
 img_typ="${VM_RUNNER_IMG_TYPE:="$(qemu-img info "$img" | sed -n 's/file format:\s*//p')"}"
-size="${VM_RUNNER_IMG_SIZE:=11G}"
+size="${VM_RUNNER_IMG_SIZE:=12G}"
 qemu-img resize -f "$img_typ" "$img" "$size"
 qemu-system-x86_64 \
 	-boot menu=off,splash-time=0,strict=on,order=c \
